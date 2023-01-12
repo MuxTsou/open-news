aececa3b7baec8d6761b8307e863565a63333e35
Mux <im885605@gmail.com>
2023-01-12 16:22:11 +0800

    [Implement] Create Dimens class

---
+++ app/src/main/java/ai/nextstop/opennews/ui/theme/Dimens.kt
@@ -0,0 +1,12 @@
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val PaddingSmall: Dp = 12.dp
+    val PaddingNormal: Dp = 16.dp
+    val PaddingLarge: Dp = 24.dp
+
+    val SpaceSmall: Dp = 4.dp
+    val SpaceNormal: Dp = 8.dp
+    val SpaceLarge: Dp = 16.dp
+}
