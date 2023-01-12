2c83eb3b5e86cd1967b7e74cd1c9fca9c80c6d5b
Mux <im885605@gmail.com>
2023-01-12 16:21:44 +0800

    [Implement] Create UiState interface

---
+++ app/src/main/java/ai/nextstop/opennews/compose/UiState.kt
@@ -0,0 +1,10 @@
+import androidx.compose.runtime.Stable
+
+@Stable
+interface UiState<T : Result<T>> {
+    val value: T?
+    val exception: Throwable?
+
+    val hasError: Boolean
+        get() = exception != null
+}
